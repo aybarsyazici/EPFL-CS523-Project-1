31adf70aba97f8586dbeeb1aec57e9a041fa39f3
cankirimca <cankirimca@gmail.com>
2023-03-09 16:20:41 +0100

    implemented add methods of share and scalar classes

--- smcompiler/secret_sharing.py
+++ smcompiler/secret_sharing.py
@@ -12,17 +12,21 @@ class Share:
     A secret share in a finite field.
     """
 
-    def __init__(self, *args, **kwargs):
-        # Adapt constructor arguments as you wish
-        raise NotImplementedError("You need to implement this method.")
+    def __init__(self, value: int, index: int):
+        self.value = value
+        self.index = index
 
     def __repr__(self):
-        # Helps with debugging.
-        raise NotImplementedError("You need to implement this method.")
+        return f"Share({self.value})"
 
     def __add__(self, other):
-        raise NotImplementedError("You need to implement this method.")
-
+        if isinstance(other, Scalar):
+            if self.index == 0:
+                return Share(0, self.value + other.value)
+            return Share(self.index, self.value)    
+        #else, both elements are shares
+        return Share(self.index, self.value + other.value)     
+        
     def __sub__(self, other):
         raise NotImplementedError("You need to implement this method.")
 
@@ -37,6 +41,20 @@ class Share:
     def deserialize(serialized) -> Share:
         """Restore object from its serialized representation."""
         raise NotImplementedError("You need to implement this method.")
+
+class Scalar:
+    def __init__(self, value: int):
+        self.value = value
+    def __add__(self, other):
+        if isinstance(other, Scalar):
+            return Scalar(self.value + other.value) 
+        #else, other one is a share    
+        #call addition operation of share class 
+        return other + self 
+    def __repr__(self):
+            return f"Scalar({self.value})"
+    
+
 
 
 def share_secret(secret: int, num_shares: int) -> List[Share]:
