5f2f093679c5668847fc06930a9b6924ffff7cc8
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2023-05-27 12:57:59 +0200

    feat: added fingerprinting.py

--- secretstroll/part3/fingerprinting.py
+++ secretstroll/part3/fingerprinting.py
@@ -2,9 +2,12 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedKFold
-
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import data_sanitize
+from tqdm import tqdm
 def classify(train_features, train_labels, test_features, test_labels):
 
     """Function to perform classification, using a 
@@ -19,13 +22,30 @@ def classify(train_features, train_labels, test_features, test_labels):
         test_labels (numpy array): list of labels (ground truth) of the test dataset
 
     Returns:
+
         predictions: list of labels predicted by the classifier for test_features
 
     Note: You are free to make changes the parameters of the RandomForestClassifier().
     """
-
+    
+    # best params from grid search
+    best_params = {
+        'n_estimators': 400,
+        'max_depth': 20,
+        'max_features': None,
+        'min_samples_leaf': 1,
+        'min_samples_split': 2,
+    }
     # Initialize a random forest classifier. Change parameters if desired.
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(**best_params)
+    # Scale the features.
+    scaler = StandardScaler()
+    train_features = scaler.fit_transform(train_features)
+    test_features = scaler.transform(test_features)
+    # Select the most important features.
+    selector = SelectFromModel(RandomForestClassifier().fit(train_features, train_labels), prefit=True)
+    train_features = selector.transform(train_features)
+    test_features = selector.transform(test_features)
     # Train the classifier using the training features and labels.
     clf.fit(train_features, train_labels)
     # Use the classifier to make predictions on the test features.
@@ -50,18 +70,43 @@ def perform_crossval(features, labels, folds=10):
     to evaluate the performance.         
     """
 
-    kf = StratifiedKFold(n_splits=folds)
-    labels = np.array(labels)
-    features = np.array(features)
+    # Initialize a stratified k-fold object for cross-validation. 
+    # You can use a different number of folds if you want.
+    skf = StratifiedKFold(n_splits=folds)
+    # Initialize lists to store the results of classification.
+    accuracy = []
+    precision = []
+    recall = []
+    f1 = []
+    i = 0
+    # Loop over the folds.
+    for train_index, test_index in tqdm(skf.split(features, labels), desc="Cross Validation"):
+        # Split the data into training and test sets.
+        train_features, test_features = np.array(features)[train_index], np.array(features)[test_index]
+        train_labels, test_labels = np.array(labels)[train_index], np.array(labels)[test_index]
+        # Perform classification.
+        predictions = classify(train_features, train_labels, test_features, test_labels)
+        # Compute metrics.
+        accuracy.append(accuracy_score(test_labels, predictions))
+        precision.append(precision_score(test_labels, predictions, average='macro'))
+        recall.append(recall_score(test_labels, predictions, average='macro'))
+        f1.append(f1_score(test_labels, predictions, average='macro'))
+        # Print the metrics for the current fold.
+        i += 1
+        print()
+        print(f"Metrics for fold {i}:")
+        print(f"\tAccuracy: {accuracy[-1]}")
+        print(f"\tPrecision: {precision[-1]}")
+        print(f"\tRecall: {recall[-1]}")
+        print(f"\tF1: {f1[-1]}")
+    # Print the average metrics.
+    print("\nAverage metrics:")
+    print("\tAccuracy: ", np.mean(accuracy))
+    print("\tPrecision: ", np.mean(precision))
+    print("\tRecall: ", np.mean(recall))
+    print("\tF1: ", np.mean(f1))
 
-    for train_index, test_index in kf.split(features, labels):
-        X_train, X_test = features[train_index], features[test_index]
-        y_train, y_test = labels[train_index], labels[test_index]
-        predictions = classify(X_train, y_train, X_test, y_test)
 
-    ###############################################
-    # TODO: Write code to evaluate the performance of your classifier
-    ###############################################
 
 def load_data():
 
@@ -90,13 +135,10 @@ def load_data():
     Note: You will have to decide what features/labels you want to use and implement 
     feature extraction on your own.
     """
-
-    ###############################################
-    # TODO: Complete this function. 
-    ###############################################
-
-    features = []
-    labels = []
+    training_data = data_sanitize.get_saved_training_data()
+    # 0th index is the label, rest are features
+    features = [x[1:] for x in training_data]
+    labels = [x[0] for x in training_data]
 
     return features, labels
         
